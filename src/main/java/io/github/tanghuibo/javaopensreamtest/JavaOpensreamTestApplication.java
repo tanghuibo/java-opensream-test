0ac02c3195ab82099bab1025087b3d401a421c0a
tang_huibo <1215893264@qq.com>
2021-02-19 21:02:46 +0800

    basic init

---
+++ src/main/java/io/github/tanghuibo/javaopensreamtest/JavaOpensreamTestApplication.java
@@ -0,0 +1,13 @@
+package io.github.tanghuibo.javaopensreamtest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaOpensreamTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaOpensreamTestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/github/tanghuibo/javaopensreamtest/JavaOpensreamTestApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.tanghuibo.javaopensreamtest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaOpensreamTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
